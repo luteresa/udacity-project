3b55d0729c19475747d1feb71f6e73cbe5047c4f
leon.yu <luteresa@163.com>
2019-02-11 22:51:24 +0800

    Update student_code.py
    按A*算法改进本项目的实现

--- step0/p7_path_plan/student_code.py
+++ step0/p7_path_plan/student_code.py
@@ -15,13 +15,7 @@ def print_path(path):
     for node in path.list:
         print(node.id,end=',')
     print("")
-class Node():
-    def __init__(self,id,state,action,cost,parent):
-        self.id = id
-        self.state = state
-        self.action = action
-        self.cost = cost
-        self.parent = parent
+    
 class PriorityQueue:
     def __init__(self):
         self._queue = []
@@ -41,7 +35,7 @@ class Frontier():
         self.goal = goal
         
     def add(self,path,M):
-        cost = path.end().cost #+distance(path.end().id,self.goal,M)
+        cost = path.end().g_cost + path.end().h_cost
         self.queue.push(path,cost)
         self.set.add(path)
         #print("add new path,cost=",cost)
@@ -57,24 +51,30 @@ class Explored():
     def __init__(self):
         self.set = set()
 class Node():
-    def __init__(self,id,state,actions,cost,parent):
+    def __init__(self,id,state,actions,g_cost,h_cost,parent):
         self.id = id
         self.state = state
         self.actions = actions
-        self.cost = cost
+        self.g_cost = g_cost
+        self.h_cost = h_cost
         self.parent = parent
 def distance(id1,id2,M):
     dist = math.sqrt(math.pow(abs( M.intersections[id1][0]- M.intersections[id2][0]),2) + math.pow(abs( M.intersections[id1][1]- M.intersections[id2][1]),2))
     return dist
-def Result(node,id,M):
-    cost = node.cost + distance(node.id,id,M)
-    return Node(id,1,M.roads[id],cost,node)
+def Result(node,id,goal,M):
+    g_cost = node.g_cost + distance(node.id,id,M)
+    h_cost = heuristic_cost_estimate(id,goal,M)
+    return Node(id,1,M.roads[id],g_cost,h_cost,node)
 def path_2_answer(path):
     answer = []
     for node in path.list:
         answer.append(node.id)
     print(answer)
     return answer
+def heuristic_cost_estimate(start,goal,M):
+    dist = math.sqrt(math.pow(abs( M.intersections[start][0]- M.intersections[goal][0]),2) + math.pow(abs( M.intersections[start][1]- M.intersections[goal][1]),2))
+    return dist
+    
 def shortest_path(M,start,goal):
     '''
     for key,value in M.intersections.items():
@@ -85,7 +85,7 @@ def shortest_path(M,start,goal):
         num += 1
     '''
     print("shortest path called")
-    node = Node(start,1,M.roads[start],0,start)
+    node = Node(start,1,M.roads[start],0,heuristic_cost_estimate(start,goal,M),start)
     frontier = Frontier()
     frontier.set_goal(goal)
     path = Path()
@@ -94,26 +94,29 @@ def shortest_path(M,start,goal):
     
     explored = Explored()
     
-    while True:
-        if frontier.queue.len() < 1:
-            return []
-      
+    while frontier.queue.len() > 0:
         path_choice = frontier.choice(M)#get from frontier
         s = path_choice.end()
+        
+        if s.id == goal:
+            return path_2_answer(path_choice)
+        
         if s.id in explored.set:   #避免多余的搜索
             continue
             
         explored.set.add(s.id)    #add s to explored
         #print("explored.set:",explored.set)
         
-        if s.id == goal:
-            return path_2_answer(path_choice)
         
         for id in s.actions:
-            node_new = Result(s,id,M)
+            if id in explored.set:
+                continue
+            node_new = Result(s,id,goal,M)
             path_new = copy.deepcopy(path_choice)
             path_new.add(node_new)
     
             #if node_new.id not in frontier.set and node_new.id not in explored.set: #add[path+a > Result(s,a)]
             if node_new.id not in explored.set:
                 frontier.add(path_new,M)    #to frontier
+     ret = []
+     return ret
