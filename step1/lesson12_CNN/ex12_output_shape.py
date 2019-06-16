966e342833af4470a84ad4020f1ff4447e81c69a
yuyunbo <luteresa@163.com>
2019-06-16 23:53:02 +0800

    add cnn

---
+++ step1/lesson12_CNN/ex12_output_shape.py
@@ -0,0 +1,10 @@
+import tensorflow as tf
+
+input = tf.placeholder(tf.float32, (None, 32, 32, 3))
+filter_weights = tf.Variable(tf.truncated_normal((8, 8, 3, 20))) # (height, width, input_depth, output_depth)
+filter_bias = tf.Variable(tf.zeros(20))
+strides = [1, 2, 2, 1] # (batch, height, width, depth)
+padding = 'SAME'
+conv = tf.nn.conv2d(input, filter_weights, strides, padding) + filter_bias
+
+print(conv)
