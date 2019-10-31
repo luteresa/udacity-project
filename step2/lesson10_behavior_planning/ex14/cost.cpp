ecde07fbf6203adbcaa4779c4d3c1cfec58d2cf6
leon <luteresa@163.com>
2019-10-31 15:29:06 +0800

    add cost function

---
+++ step2/lesson10_behavior_planning/ex14/cost.cpp
@@ -0,0 +1,20 @@
+#include "cost.h"
+#include <cmath>
+
+double goal_distance_cost(int goal_lane, int intended_lane, int final_lane, 
+                          double distance_to_goal) {
+  // The cost increases with both the distance of intended lane from the goal
+  //   and the distance of the final lane from the goal. The cost of being out 
+  //   of the goal lane also becomes larger as the vehicle approaches the goal.
+    
+  /**
+   * TODO: Replace cost = 0 with an appropriate cost function.
+   */
+  double cost = 0;
+
+  
+  cost = 1 - exp(-2.0*(std::abs(goal_lane - intended_lane - final_lane)/distance_to_goal));
+
+    
+  return cost;
+}
